e4debf25defb41f322134e18ee0c2841125348a7
Michael Ernest <m.hendersun@gmail.com>
2020-09-25 12:50:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,32 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip.to_i - 1
-  index = input
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
-
-def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    end
-  end
-
-end
-
-def position_taken?(board, index)
-  board[index] != " "
-end
-
-def move(board, index, current_player = "X")
-  board[index] = current_player
-end
