e88d75c2e1919fa090ffda63ab11f332ccdf07e4
Michael Ernest <m.hendersun@gmail.com>
2020-09-25 13:15:53 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,7 +26,7 @@ def position_taken?(board, index)
   board[index] != " "
 end
 
-def turn(board, index)
+def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
   index = input_to_index(user_input)
